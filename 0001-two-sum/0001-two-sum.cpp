530cb173c81e44d96e3682d4878cfa54a33dfe2a
Aman Sharma <143827716+AmanVilvas@users.noreply.github.com>
2025-06-05 08:34:16 +0530

    Time: 43 ms (29.36%), Space: 14.1 MB (63.89%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
-    Solution() {
-        ios_base ::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
-    }
     vector<int> twoSum(vector<int>& nums, int target) {
-        // Declare a hash map to store the numbers and their indices
-        unordered_map<int, int> mp;
-        // Loop through the array
-        for (int i = 0; i < nums.size(); i++) {
-            // Calculate the complement of the current number
-            int complement = target - nums[i];
-            // Check if the complement exists in the hash map
-            if (mp.count(complement)) {
-                // If yes, return the indices of the current number and its
-                // complement
-                return {mp[complement], i};
+        // int sum = nums[0];
+        for(int i=0; i<nums.size()-1; i++){
+            for(int j=i+1; j<nums.size(); j++){
+               if(nums[i] + nums[j] == target){
+                return {i,j};
+               } 
             }
-            // If not, add the current number and its index to the hash map
-            mp[nums[i]] = i;
         }
-        // If no pair is found, return an empty vector as a default value
-        return {-1, -1};
+        return {};
     }
 };
