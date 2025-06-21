51ba92741e9f7a124e99611241eaca16558813bc
Aman Sharma <143827716+AmanVilvas@users.noreply.github.com>
2025-06-21 16:02:20 +0530

    Time: 0 ms (100%), Space: 18.6 MB (50.98%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m =  matrix.size(), n = matrix[0].size();
+        int colo = 1;
+      for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+
+          if(matrix[i][j] == 0){
+            //i-th row ko zero karo
+
+            matrix[i][0] = 0;
+            //now col ko zero mark karo
+
+            if(j != 0){ // it means 1st nhi hai to overlap nho karega
+                matrix[0][j] = 0;
+            } else {
+                colo = 0;
+            }
+          }
+        }
+      } 
+
+            for(int i=1 ; i<m; i++){
+                for(int j=1; j<n; j++){
+                    if(matrix[i][j] != 0){
+                        if(matrix[0][j] == 0 || matrix[i][0] == 0){
+                            matrix[i][j] = 0;
+                        }
+                    }
+                }   
+            }
+            if (matrix[0][0] == 0) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if (colo == 0) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+      
+    }
+};
