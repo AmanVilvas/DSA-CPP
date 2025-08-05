6e641d7feead83b22a07ee5fed065f618177f62d
Aman Sharma <143827716+AmanVilvas@users.noreply.github.com>
2025-08-05 23:11:21 +0530

    Time: 0 ms (100%), Space: 17.5 MB (52.28%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,39 +1,50 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& arr, int target) {
-        vector<int>ans;
-        int low = 0, high = arr.size()-1;
+        int low = 0, high = arr.size() - 1; 
+        int first = -1, sec = -1;
+
+        //first occ
+        first = binaryLeft(arr, 0, arr.size() - 1, target);
+
+        // if first is found
+        if (first != -1) {
+            sec = binaryRight(arr, first, arr.size() - 1, target);
+        }
+
+        return {first, sec};
+    }
+
+private:
+    int binaryLeft(vector<int>& arr, int l, int h, int x) {
         int first = -1;
-        while(low <= high){
-            int mid = low + (high-low)/2;
-            if(arr[mid] == target){
-                 first = mid;
-                high = mid - 1;
-            }else if (arr[mid] > target){
-                high = mid - 1;
-            }else {
-                low = mid + 1;
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+            if (arr[mid] == x) {
+                first = mid;
+                h = mid - 1; // search left
+            } else if (arr[mid] < x) {
+                l = mid + 1;
+            } else {
+                h = mid - 1;
             }
         }
-            ans.push_back(first);
+        return first;
+    }
 
-         low = 0, high = arr.size()-1;
-        int last = -1;
-        while(low <= high){
-            int mid = low + (high-low)/2;
-            if(arr[mid] == target){
-                last = mid;
-                low = mid + 1;
-            }else if (arr[mid] > target){
-                high = mid - 1;
-            }else {
-                low = mid + 1;
+    int binaryRight(vector<int>& arr, int l, int h, int x) {
+        int sec = -1;
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+            if (arr[mid] == x) {
+                sec = mid;
+                l = mid + 1; // search  right
+            } else if (arr[mid] < x) {
+                l = mid + 1;
+            } else {
+                h = mid - 1;
             }
         }
-        ans.push_back(last);
-
-        return ans;
-
-} 
-    
+        return sec;
+    }
 };
