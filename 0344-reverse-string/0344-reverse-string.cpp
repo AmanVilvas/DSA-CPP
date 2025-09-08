66f5a3add236959c5ff66f4c445f9000406f4d5e
Aman Sharma <143827716+AmanVilvas@users.noreply.github.com>
2025-09-08 20:39:19 +0530

    Time: 4 ms (4.45%), Space: 27.3 MB (16.9%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int i = 0, j = s.size()-1;
+        solve(s, i, j);
+    }
+    void solve (vector<char>& s, int i, int j){
+        if(i>=j){
+            return ;
+        }
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+        solve(s, i+1,j-1);
+
+    }
+};
