0f298192b1a6524cf4346fbddf65fa7af4573e4c
Aman Sharma <143827716+AmanVilvas@users.noreply.github.com>
2025-12-05 08:12:28 +0530

    Time: 0 ms (100%), Space: 8.4 MB (92.06%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -2,22 +2,28 @@ class Solution {
 public:
 
     double pow(double x, long n){
-        if(n == 1){
+
+        if(x == 0){
+            return 0;
+        }if( n == 0){
+            return 1;
+        }if(n == 1){
             return x;
         }
-        if(n == 0){
-            return 1.0;
-        }
-        if(n%2 == 0){
+        if(n % 2 == 0){
             return pow(x*x, n/2);
         }
-            return x * pow(x, n-1);
+        return x * pow(x, n-1);
         
+
     }
 
     double myPow(double x, long n) {
-        if(n < 0){
-            return 1/pow(x, -1*n); 
-        }return pow(x,n);
+        long num = n;
+    //    long ans = ;
+       if(num < 0){
+        return 1/pow(x, -1*n);
+       } 
+       return pow(x, n);
     }
 };
